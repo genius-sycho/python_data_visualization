2ea93ee9f651ac4029483bc187cb5e32e96ce89f
genius-sycho <abdullahumer7519@gmail.com>
2025-06-16 23:41:06 +0500

    Sales_linechart.py

--- Sales_linechart.py
+++ Sales_linechart.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Step 1: Load the CSV file
+# Step 1: Loading the CSV file
 data = pd.read_csv('sales.csv')
 
-# Step 2: Plot the data
+# Step 2: Plotting the data
 plt.plot(data['Month'], data['Sales'], marker='o', linestyle='-', color='teal')
 
-# Step 3: Customize the chart
+# Step 3: Customizing the chart
 plt.title('Monthly Sales')
 plt.xlabel('Month')
 plt.ylabel('Sales in USD')
 plt.grid(True)
 
-# Step 4: Show the chart
+# Step 4: Showing the chart
 plt.show()
